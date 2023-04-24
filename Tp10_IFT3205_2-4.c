fdbb1720aea3a2242c23bfe52a5334495507f728
Therk <43972622+Therrk@users.noreply.github.com>
2023-04-23 22:58:34 -0400

    question 4

---
+++ Tp10_IFT3205_2-4.c
@@ -0,0 +1,101 @@
+/*------------------------------------------------------*/
+/* Prog    : Tp10_IFT3205.c                             */
+/* Auteur  : Élie Leblanc, Justin Veilleux              */
+/* Date    : --/--/2010                                 */
+/* version :                                            */ 
+/* langage : C                                          */
+/* labo    : DIRO                                       */
+/*------------------------------------------------------*/
+// elie.leblanc@umontreal.ca
+
+
+/*------------------------------------------------*/
+/* FICHIERS INCLUS -------------------------------*/
+/*------------------------------------------------*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "FonctionDemo10.h"
+
+/*------------------------------------------------*/
+/* DEFINITIONS -----------------------------------*/   
+/*------------------------------------------------*/
+#define NAME_VISUALISER_IMG "./display "
+#define NAME_VISUALISER     "./ViewSig.sh "
+
+/*------------------------------------------------*/
+/* PROTOTYPE DE FONCTIONS  -----------------------*/   
+/*------------------------------------------------*/
+
+/*------------------------------------------------*/
+/* PROGRAMME PRINCIPAL   -------------------------*/                     
+/*------------------------------------------------*/
+int main(int argc,char **argv)
+ {
+  int i,j,n;
+  char BufSystVisuSig[100];
+  int length;
+
+  //=================================================
+  //Question 2.1
+  //------------
+  //
+  // Sample Rate = 11025 ech/sec
+  // Frequence �chant= 11025
+  //
+  //  
+  //=================================================
+
+  float*  SignX=LoadSignalDat("SOUND_GoodMorningVietnam",&length);
+  float*  SignY=fmatrix_allocate_1d(length);
+
+
+  //--------------------------------
+  //Restauration  �quation R�curente
+  //--------------------------------
+  // 
+  // y(n) = x(n) + G . x(n-Retard)
+  //
+  //--------------------------------
+
+  // on veut trouver le n0 pour que la différence de fréquence soit de
+  // 784 Hz, comme on as 11025 échantillons par seconde, 11025/784 = 14
+
+  float SamplingRate=11025;
+  int   Retard=14;
+  float max=0;
+
+  for(n=0;n<length;n++){
+    SignX[n]=(float)rand();
+    if (SignX[n]>max) {
+	    max = SignX[n];
+    }
+  }
+  for(n=0;n<length;n++){
+    SignX[n]/=max;
+    if (n>=Retard) {
+	      SignY[n]= SignX[n]+SignX[n-Retard];
+    }
+  }
+     
+   //Sauvegarde
+   SaveSignalDatWav("white",SignY,length,SamplingRate); 
+   //SaveSignalDat("SOUND_GoodMorningVietnam1",SignY,length);
+ 
+   //Visu
+   //strcpy(BufSystVisuSig,NAME_VISUALISER);
+   //strcat(BufSystVisuSig,"SOUND_GoodMorningVietnam1.dat&");
+   //printf(" %s",BufSystVisuSig);
+   //system(BufSystVisuSig);
+       
+ 
+  //==End=========================================================
+
+  //retour sans probleme
+  printf("\n C'est fini ... \n\n");
+  return 0; 	 
+}
+
+
