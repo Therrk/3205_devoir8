2a85660fbcdf403a831261675922bed6a8cebbe8
Therk <43972622+Therrk@users.noreply.github.com>
2023-04-23 22:38:25 -0400

    question 3

---
+++ Tp10_IFT3205_2-3.c
@@ -0,0 +1,97 @@
+/*------------------------------------------------------*/
+/* Prog    : Tp10_IFT3205.c                             */
+/* Auteur  : Élie Leblanc, Justin Veilleux              */
+/* Date    : --/--/2010                                 */
+/* version :                                            */ 
+/* langage : C                                          */
+/* labo    : DIRO                                       */
+/*------------------------------------------------------*/
+// elie.leblanc@umontreal.ca
+
+
+/*------------------------------------------------*/
+/* FICHIERS INCLUS -------------------------------*/
+/*------------------------------------------------*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "FonctionDemo10.h"
+
+/*------------------------------------------------*/
+/* DEFINITIONS -----------------------------------*/   
+/*------------------------------------------------*/
+#define NAME_VISUALISER_IMG "./display "
+#define NAME_VISUALISER     "./ViewSig.sh "
+
+/*------------------------------------------------*/
+/* PROTOTYPE DE FONCTIONS  -----------------------*/   
+/*------------------------------------------------*/
+
+/*------------------------------------------------*/
+/* PROGRAMME PRINCIPAL   -------------------------*/                     
+/*------------------------------------------------*/
+int main(int argc,char **argv)
+ {
+  int i,j,n;
+  char BufSystVisuSig[100];
+  int length;
+
+  //=================================================
+  //Question 2.1
+  //------------
+  //
+  // Sample Rate = 11025 ech/sec
+  // Frequence �chant= 11025
+  //
+  //  
+  //=================================================
+
+  float*  SignX=LoadSignalDat("SOUND_GoodMorningVietnam",&length);
+  float*  SignY=fmatrix_allocate_1d(length);
+  float*  reponse_Impul=fmatrix_allocate_1d(length);
+  float*  impul=fmatrix_allocate_1d(length);
+
+
+  //--------------------------------
+  //Restauration  �quation R�curente
+  //--------------------------------
+  // 
+  // y(n) = x(n) + G . x(n-Retard)
+  //
+  //--------------------------------
+  float SamplingRate=11025;
+  float G=0.9;
+  int   Retard=2205;
+
+  impul[0]=1;
+
+  for(n=0;n<length;n++){
+    SignY[n]=0.0;
+    if (n>Retard-1) {
+      SignY[n]+=-G*SignX[n]+SignX[n-Retard]+G*SignY[n-Retard];
+      reponse_Impul[n]=-G*impul[n]+impul[n-Retard]+G*reponse_Impul[n-Retard];
+    }
+  }
+     
+   //Sauvegarde
+   SaveSignalDatWav("SOUND_GoodMorningVietnam1",SignY,length,SamplingRate); 
+   SaveSignalDatWav("reponse_Impul",reponse_Impul,length,SamplingRate);
+   //SaveSignalDat("SOUND_GoodMorningVietnam1",SignY,length);
+ 
+   //Visu
+   //strcpy(BufSystVisuSig,NAME_VISUALISER);
+   //strcat(BufSystVisuSig,"SOUND_GoodMorningVietnam1.dat&");
+   //printf(" %s",BufSystVisuSig);
+   //system(BufSystVisuSig);
+       
+ 
+  //==End=========================================================
+
+  //retour sans probleme
+  printf("\n C'est fini ... \n\n");
+  return 0; 	 
+}
+
+
